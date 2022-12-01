f57f2c81a6324e40bb877eb7974ca5d8588d9a4a
Phillip <pgkb78@umsystem.edu>
2022-12-01 15:34:20 -0600

    1st commit

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const placesRoutes = require("./routes/places-routes");
+const usersRoutes = require("./routes/users-routes");
+const HttpError = require("./models/http-error");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  //don't send back a response, but add certain headers to response.  so when a response is sent, it has headers attached
+  // * allows control in which domains should have access.  In this case, 'any domain' localhost3000 and localhost5000
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  next();
+});
+
+app.use("/api/places", placesRoutes);
+// routes configured in places-routes are added as middleware in app.js
+//  allows logic to be moved out of app.js, keep file lean, and set up place-related routes IN the place-routes file
+app.use("/api/users", usersRoutes);
+
+app.use((req, res, next) => {
+  const error = new HttpError("Could not find this route.", 404);
+  throw error;
+});
+
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({ message: error.message || "An unknown error occurred!" });
+});
+
+mongoose
+  .connect(
+    "mongodb+srv://3816:fQTZGDJHpPUnLB3V@cluster0.rmeshhn.mongodb.net/mern?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    app.listen(5000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
